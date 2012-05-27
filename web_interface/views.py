f2e7e3c24593aa1b97fd76658ced691fe0a7e11a
Raphael Cruzeiro <raphaelcruzeiro@raphaelcruzeiro.com>
2012-05-27 04:35:44 -0300

    Thread synchronization

--- web_interface/views.py
+++ web_interface/views.py
@@ -8,8 +8,11 @@ import hashlib
 import os
 import sys
 import threading
+from threading import Lock
 import shlex
 from datetime import datetime
+
+lock = Lock()
 
 def index(request):
     return render_to_response('index.html', { 'form' : PreviewRequestForm(), 'year' : datetime.now().year })
@@ -23,40 +26,45 @@ def submit_generation_request(request):
 
             class Worker(threading.Thread):
                 def run(self):
-                    print 'Initiating worker'
-                    requests = PreviewRequest.objects.filter(generated=False).all()
+                    lock.acquire()
+
+                    try:
+                        print 'Initiating worker'
+                        requests = PreviewRequest.objects.filter(generated=False).all()
 
 
-                    PROJECT_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../media')
-                    macos = '../bin/macos/webimage'
-                    linux64 = '../bin/linux64/webimage'
+                        PROJECT_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../media')
+                        macos = '../bin/macos/webimage'
+                        linux64 = '../bin/linux64/webimage'
 
-                    webimage_path = \
-                        os.path.join(os.path.abspath(os.path.dirname(__file__)), macos) if sys.platform == 'darwin' \
+                        webimage_path =\
+                        os.path.join(os.path.abspath(os.path.dirname(__file__)), macos) if sys.platform == 'darwin'\
                         else os.path.join(os.path.abspath(os.path.dirname(__file__)), linux64)
 
 
-                    for r in requests:
-                        img_name = '%s.jpg' % hashlib.sha1('%s%s' % (r.url, str(r.requested_on))).hexdigest()
-                        img_path = os.path.join(PROJECT_PATH, img_name)
-                        command = []
-                        if sys.platform == 'darwin':
-                            command = shlex.split('%s %s %s 1024' % (webimage_path, r.url, img_path))
-                        elif sys.platform == 'linux2' or sys.platform == 'linux':
-                            command = shlex.split('xvfb-run --server-args="-screen 0, 1024x768x24" %s %s %s 1024' % (webimage_path, r.url, img_path))
-                        print command
-                        if subprocess.call(command):
-                            print 'Error!'
-                            return
-                        r.image_path = '/media/%s' % img_name
-                        r.generated = True
-                        r.save()
+                        for r in requests:
+                            img_name = '%s.jpg' % hashlib.sha1('%s%s' % (r.url, str(r.requested_on))).hexdigest()
+                            img_path = os.path.join(PROJECT_PATH, img_name)
+                            command = []
+                            if sys.platform == 'darwin':
+                                command = shlex.split('%s %s %s 1024' % (webimage_path, r.url, img_path))
+                            elif sys.platform == 'linux2' or sys.platform == 'linux':
+                                command = shlex.split('xvfb-run --server-args="-screen 0, 1024x768x24" %s %s %s 1024' % (webimage_path, r.url, img_path))
+                            print command
+                            if subprocess.call(command):
+                                print 'Error!'
+                                return
+                            r.image_path = '/media/%s' % img_name
+                            r.generated = True
+                            r.save()
 
-                        send_mail('The Website preview generator tool',
-                            'Hello there,\nThe website preview you requested is ready. '
-                            'Visit the following address to see the generated preview:'
-                            '\n http://previewtool.raphaelcruzeiro.com%s' % r.image_path,
-                            'noreply@raphaelcruzeiro.com', [r.email], fail_silently=True,)
+                            send_mail('The Website preview generator tool',
+                                'Hello there,\nThe website preview you requested is ready. '
+                                'Visit the following address to see the generated preview:'
+                                '\n http://previewtool.raphaelcruzeiro.com%s' % r.image_path,
+                                'noreply@raphaelcruzeiro.com', [r.email], fail_silently=True,)
+                    finally:
+                        lock.release()
 
 
             Worker().start()
